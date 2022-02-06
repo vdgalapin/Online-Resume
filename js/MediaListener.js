70ea181c9bb50a746d97105a1e6cbe929991c80e
Vyron Galapin <59787986+vdgalapin@users.noreply.github.com>
2022-02-06 13:54:26 -0600

    Switch Div Position JS

---
+++ js/MediaListener.js
@@ -0,0 +1,28 @@
+// Create a condition that targets viewports at most 991px wide
+const mediaQuery1 = window.matchMedia('(max-width: 991px)');
+// Create a condition that targets viewports at least 992px wide
+const mediaQuery2 = window.matchMedia('(min-width: 992px)');
+function handleTabletChange(e) {
+  // Check if the media query is true
+  if (e.matches) {
+    div1 = jQuery('.DivOne');
+    div2 = jQuery('.DivTwo');
+    
+    tdiv1 = div1.clone();
+    tdiv2 = div2.clone();
+    
+    if(!div2.is(':empty')){
+      div1.replaceWith(tdiv2);
+      div2.replaceWith(tdiv1);    
+    }
+
+  }
+}
+
+// Register event listener
+mediaQuery1.addListener(handleTabletChange);
+mediaQuery2.addListener(handleTabletChange);
+
+// Initial check
+handleTabletChange(mediaQuery1);
+handleTabletChange(mediaQuery2);
