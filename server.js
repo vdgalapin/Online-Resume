eece42f3eebf3bf4877b24674b43f132776e632d
Vyron Galapin <59787986+vdgalapin@users.noreply.github.com>
2022-03-12 08:33:29 -0600

    heroku

---
+++ server.js
@@ -0,0 +1,32 @@
+// Load Node modules
+var express = require('express');
+
+// Add the EJS Node Moduleto the server
+const ejs = require('ejs');
+
+// Initialise Express
+var app = express();
+
+// Render static files
+app.use(express.static('webpage'));
+
+// Set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// Port website will run on
+app.listen(8080);
+
+// *** GET Routes - display pages ***
+app.get('/', function(req, res) {
+	var Dad = "Rosvic";
+	var Mom = "Julifer";
+	var Kids = ["Vyron", "Jag", "Jewl", "Vince"];
+
+	// Render index page
+	res.render('pages/index', {
+		// EJS variable and server-side variable
+		Dad: Dad
+		Mom: Mom
+		Kids:  kids
+	});
+});
